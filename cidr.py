826d94968dd0d595d20aaee2ecbbb02dd7fa9458
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 23:54:37 +0300

    heavy ranges

--- cidr.py
+++ cidr.py
@@ -4,7 +4,31 @@ import ipaddress
 from termcolor import colored
 
 def scan_cidr(cidr, port):
-    ip_network = ipaddress.ip_network(cidr)
+    try:
+        ip_network = ipaddress.ip_network(cidr)
+    except ValueError:
+        print(colored("Invalid CIDR range. Please enter a valid range.", "red"))
+        print(colored("Example: 192.168.0.0/24", "yellow"))
+        return
+
+    num_hosts = ip_network.num_addresses
+
+    if num_hosts > 10000:
+        confirm = input(colored(f"The provided CIDR range has a large number of hosts ({num_hosts}). "
+                                "Scanning such a range may take a long time. "
+                                "Do you still want to proceed? (y/n): ", "yellow"))
+        if confirm.lower() != 'y':
+            print(colored("Scanning operation cancelled.", "yellow"))
+            return
+
+    try:
+        int_port = int(port)
+        if int_port < 1 or int_port > 65535:
+            raise ValueError
+    except ValueError:
+        print(colored("Invalid port number. Please enter a valid port.", "red"))
+        print(colored("Example: 80", "yellow"))
+        return
 
     print(colored(r'''
   ____ ___ ____  ____  ____            _          
@@ -13,14 +37,14 @@ def scan_cidr(cidr, port):
 | |___ | || |_| |  _ <|  __/| | | (_) | |_) |  __/
  \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|
                                                   ''', "white"))
-    print(colored("contact me via telegram for any issues ", "white"))
+    print(colored("Contact me via Telegram for any issues.", "white"))
     print(colored("https://t.me/brian_72", "white"))
     print()
 
-    print(colored("IP                Port  -Status   HTTP -Status", attrs=["bold"]))
-    print("----------------------------------------------")
+    print(colored("IP                Port  -Status   HTTP -Status  Location", attrs=["bold"]))
+    print("---------------------------------------------------------")
 
-    for ip in ip_network:
+    for ip in ip_network.hosts():
         ip_str = str(ip)
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,12 +59,11 @@ def scan_cidr(cidr, port):
 
             url = f"http://{ip_str}:{port}"
             try:
-                response = requests.get(url, timeout=3)
+                response = requests.get(url, allow_redirects=False, timeout=3)
                 http_status = response.status_code
-                http_status_text = colored(http_status, "blue")
                 http_response = response.reason
-                http_response_text = colored(http_response, "yellow")
-                print(f"{ip_str:<17} {port:<6} {port_status:<8} {http_status_text:<10} {http_response_text}")
+                location = response.headers.get('Location', "")
+                print(f"{ip_str:<17} {port:<6} {port_status:<8} {http_status:<10} {http_response:<10} {location}")
             except requests.exceptions.RequestException:
                 print(f"{ip_str:<17} {port:<6} {port_status:<8} {colored('N/A', 'blue'):<10} {colored('N/A', 'yellow')}")
 
@@ -49,6 +72,14 @@ def scan_cidr(cidr, port):
             break
 
 def main():
+    print("CIDRProbe - IP Range Scanner")
+    print()
+
+    print("Usage Example:")
+    print("CIDR Range: 192.168.0.0/24")
+    print("Port: 80")
+    print()
+
     cidr = input("Enter the CIDR range: ")
     port = input("Enter the port to scan: ")
 
