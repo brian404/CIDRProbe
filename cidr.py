a26591c11104b428515cb1d066350d8bdb948b0a
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 16:45:10 +0300

    /

--- cidr.py
+++ cidr.py
@@ -13,43 +13,38 @@ def scan_cidr(cidr, port):
     for ip in ip_network:
         ip_str = str(ip)
         try:
-            if nm[ip_str].has_tcp(int(port)) and nm[ip_str]['tcp'][int(port)]['state'] == 'open':
-                port_status = colored("Open", "green")
+            if nm[ip_str].has_tcp(port) and nm[ip_str]['tcp'][port]['state'] == 'open':
+                status = colored("Open", "green")
             else:
-                port_status = colored("Closed", "red")
-
+                status = colored("Closed", "red")
+            
             url = f"http://{ip_str}:{port}"
-            try:
-                response = requests.get(url, timeout=3)
-                http_status = response.status_code
-                if http_status == 200:
-                    http_status = colored(http_status, "green")
-                else:
-                    http_status = colored(http_status, "yellow")
-                print(f"{ip_str:<17} {port:<6} {port_status:<8} {http_status:<10}")
-            except requests.exceptions.RequestException:
-                print(f"{ip_str:<17} {port:<6} {port_status:<8} {'N/A':<10}")
-
-        except KeyboardInterrupt:
-            print("\nOperation cancelled by user.")
-            break
+            response = requests.get(url, timeout=3)
+            http_status = response.status_code
+            if http_status == 200:
+                http_status = colored(http_status, "green")
+            else:
+                http_status = colored(http_status, "yellow")
+            
+            print(f"{ip_str:<17} {port:<6} {status:<8} {http_status:<10}")
+        
+        except requests.exceptions.RequestException:
+            print(f"{ip_str:<17} {port:<6} {status:<8} {'N/A':<10}")
 
 def main():
-    print('''
- ____ ___ ____  ____  ____            _          
-/ ___|_ _|  _ \|  _ \|  _ \ _ __ ___ | |__   ___ 
-| |    | || | | | |_) | |_) | '__/ _ \| '_ \ / _ \
-| |___ | || |_| |  _ <|  __/| | | (_) | |_) |  __/
- \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|
-                                                  
-    ''')
-    print("Use this banner and my Telegram link for contact purpose")
-    print("https://t.me/brian_72")
-
     cidr = input("Enter the CIDR range: ")
     port = input("Enter the port to scan: ")
 
-    scan_cidr(cidr, port)
+    try:
+        scan_cidr(cidr, port)
+    except KeyboardInterrupt:
+        print("\nOperation cancelled by user.")
 
 if __name__ == "__main__":
+    print('''
+    IP                Port  -Status   HTTP -Status
+    ----------------------------------------------
+    ''')
+    print("Use this banner and my Telegram link for contact purpose")
+    print("https://t.me/brian_72")
     main()
