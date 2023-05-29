11f34cf6e0473ac44a512d3a9f07c15aefa374c2
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 16:17:51 +0300

    ,

--- cidr.py
+++ cidr.py
@@ -6,39 +6,40 @@ import nmap
 def scan_cidr(cidr, port):
     ip_network = ipaddress.ip_network(cidr)
     nm = nmap.PortScanner()
-    
-    print("IP                Port   Status      HTTP Status")
-    print("------------------------------------------------")
-    
+
+    print("IP                Port   Status   HTTP Status")
+    print("----------------------------------------------")
+
     try:
         for ip in ip_network.hosts():
             ip_str = str(ip)
-            
+
             try:
                 nm.scan(ip_str, arguments=f"-p {port}")
                 if nm[ip_str].has_tcp(int(port)) and nm[ip_str]['tcp'][int(port)]['state'] == 'open':
-                    status = colored('Open', 'green')
+                    port_status = colored('Open', 'green')
                 else:
-                    status = colored('Closed', 'blue')
-                
+                    port_status = colored('Closed', 'blue')
+
                 try:
                     response = requests.get(f"http://{ip_str}:{port}", timeout=2)
-                    http_status = colored(response.status_code, 'green')
-                    print(f"{ip_str:<18}{port:<6}{status:<12}{http_status}")
+                    http_status = response.status_code
+                    http_status_text = colored(http_status, 'green') if response.ok else colored(http_status, 'red')
+                    print(f"{ip_str:<18}{port:<6}{port_status:<8}{http_status_text}")
                     
                 except requests.exceptions.RequestException:
-                    print(f"{ip_str:<18}{port:<6}{status:<12}N/A")
-                    
+                    print(f"{ip_str:<18}{port:<6}{port_status:<8}N/A")
+
             except nmap.PortScannerError:
                 print(f"{ip_str:<18}{port:<6}N/A{'N/A'}")
-                
+
     except KeyboardInterrupt:
         print("\nOperation cancelled by user.")
-            
+
 def main():
     cidr = input("Enter the CIDR range: ")
     port = input("Enter the port to scan: ")
-    
+
     scan_cidr(cidr, port)
 
 if __name__ == "__main__":
@@ -50,6 +51,6 @@ if __name__ == "__main__":
  \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|
                                                   
 https://t.me/brian_72
-contact me via telegram for any issues
+contact me via telegram for any issues 
 ''')
     main()
