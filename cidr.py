c05589c9defb8ea7fbc7e6b43f3554004a6fa508
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 16:12:22 +0300

    /

--- cidr.py
+++ cidr.py
@@ -7,35 +7,33 @@ def scan_cidr(cidr, port):
     ip_network = ipaddress.ip_network(cidr)
     nm = nmap.PortScanner()
     
-    print("IP                Port   Status      HTTP Status   HTTP Response")
-    print("-------------------------------------------------------------")
+    print("IP                Port   Status      HTTP Status")
+    print("------------------------------------------------")
     
-    for ip in ip_network.hosts():
-        ip_str = str(ip)
-        
-        try:
-            nm.scan(ip_str, arguments=f"-p {port}")
-            if nm[ip_str].has_tcp(int(port)) and nm[ip_str]['tcp'][int(port)]['state'] == 'open':
-                status = colored('Open', 'green')
-            else:
-                status = colored('Closed', 'blue')
+    try:
+        for ip in ip_network.hosts():
+            ip_str = str(ip)
             
             try:
-                response = requests.get(f"http://{ip_str}:{port}", timeout=2)
-                if response.status_code == 200:
-                    http_status = colored(response.status_code, 'green')
-                    http_message = colored(f'({response.reason})', 'yellow')
+                nm.scan(ip_str, arguments=f"-p {port}")
+                if nm[ip_str].has_tcp(int(port)) and nm[ip_str]['tcp'][int(port)]['state'] == 'open':
+                    status = colored('Open', 'green')
                 else:
+                    status = colored('Closed', 'blue')
+                
+                try:
+                    response = requests.get(f"http://{ip_str}:{port}", timeout=2)
                     http_status = colored(response.status_code, 'green')
-                    http_message = colored(f'({response.reason})', 'yellow')
+                    print(f"{ip_str:<18}{port:<6}{status:<12}{http_status}")
                     
-                print(f"{ip_str:<18}{port:<6}{status:<12}{http_status:<14}{http_message}")
+                except requests.exceptions.RequestException:
+                    print(f"{ip_str:<18}{port:<6}{status:<12}N/A")
+                    
+            except nmap.PortScannerError:
+                print(f"{ip_str:<18}{port:<6}N/A{'N/A'}")
                 
-            except requests.exceptions.RequestException:
-                print(f"{ip_str:<18}{port:<6}{status:<12}{'N/A':<14}{'N/A'}")
-                
-        except nmap.PortScannerError:
-            print(f"{ip_str:<18}{port:<6}{'N/A':<12}{'N/A':<14}{'N/A'}")
+    except KeyboardInterrupt:
+        print("\nOperation cancelled by user.")
             
 def main():
     cidr = input("Enter the CIDR range: ")
@@ -52,6 +50,6 @@ if __name__ == "__main__":
  \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|
                                                   
 https://t.me/brian_72
-contact me via telegram for any issues 
+contact me via telegram for any issues
 ''')
     main()
