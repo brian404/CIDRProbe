c4b69504cc9b513eb183b9cf34dd87f4e0ee717c
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 14:41:31 +0300

    update

--- cidr.py
+++ cidr.py
@@ -11,15 +11,24 @@ def scan_cidr(cidr, port=80):
             status = response.status_code
             if status == 200:
                 status_text = colored("200 OK", "green")
+                port_text = colored(port, "green")
             else:
                 status_text = colored(f"{status}", "magenta")  # Use magenta as a fallback color
+                port_text = colored(port, "red")
         except requests.exceptions.RequestException:
             status_text = colored("Failed to connect", "red")
-        print(f"{str(ip):<15}{port:<7}{status_text}")
+            port_text = colored(port, "red")
+        print(f"{str(ip):<15}{port_text:<7}{status_text}")
 
 def main():
     print("CIDR Probe - Scan CIDR Range\n")
-    cidr = input("Enter the CIDR range: ")
+    while True:
+        cidr = input("Enter the CIDR range (e.g., 192.168.0.0/24): ")
+        try:
+            ip_network = ipaddress.ip_network(cidr)
+            break
+        except ValueError:
+            print("Invalid CIDR range. Please try again.\n")
     port = input("Enter the port to scan (default is 80): ")
     if not port:
         port = 80
