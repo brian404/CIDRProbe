ca3fde7fc7d136f08aa3565ee94d17fa978d9da8
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 15:58:26 +0300

    /

--- cidr.py
+++ cidr.py
@@ -1,42 +1,64 @@
+from termcolor import colored
+import requests
 import ipaddress
-import socket
+import nmap
 
 def scan_cidr(cidr, port):
     ip_network = ipaddress.ip_network(cidr)
-    print(f"Scanning CIDR range: {cidr}")
-    print(f"Port: {port}\n")
-    print("IP\t\tPort\tStatus")
-    print("--------------------------------------")
-
+    nm = nmap.PortScanner()
+    
     for ip in ip_network.hosts():
         ip_str = str(ip)
+        
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.settimeout(2)  # Set a timeout for the connection attempt
-                result = sock.connect_ex((ip_str, port))
-                if result == 0:
-                    print(f"{ip_str}\t{port}\tOpen")
+            nm.scan(ip_str, arguments=f"-p {port}")
+            if nm[ip_str].has_tcp(port) and nm[ip_str]['tcp'][port]['state'] == 'open':
+                status = colored('Open', 'green')
+            else:
+                status = colored('Closed', 'blue')
+            
+            try:
+                response = requests.get(f"http://{ip_str}:{port}", timeout=2)
+                if response.status_code == 200:
+                    http_status = colored(response.status_code, 'green')
+                    http_message = colored('(OK)', 'yellow')
                 else:
-                    print(f"{ip_str}\t{port}\tClosed")
-        except socket.error:
-            print(f"{ip_str}\t{port}\tUnable to scan")
+                    http_status = colored(response.status_code, 'green')
+                    http_message = colored(f'({response.reason})', 'yellow')
+            except requests.exceptions.RequestException:
+                http_status = colored('Error', 'red')
+                http_message = 'Connection Error'
+            
+            print(f"{colored(ip_str, 'white', attrs=['bold']):<15}{port:<7}{status:<13}{http_status:<10}{http_message}")
+        
+        except KeyboardInterrupt:
+            print("\nOperation cancelled by user.")
+            break
 
 def main():
     cidr = input("Enter the CIDR range: ")
-    port = input("Enter the port to scan (default is 80): ") or 80
-    port = int(port)
+    port = input("Enter the port to scan (default is 80): ") or '80'
 
-    scan_cidr(cidr, port)
+    try:
+        ip_network = ipaddress.ip_network(cidr)
+        print(f"\nScanning CIDR range: {cidr}")
+        print(f"Port: {port}\n")
+        print("IP             Port   Status     HTTP Status     HTTP Response")
+        print("----------------------------------------------------------------")
+        scan_cidr(cidr, port)
+    
+    except ValueError:
+        print("Invalid CIDR range input. Please enter a valid CIDR range.")
 
 if __name__ == "__main__":
-    print(r'''
+    print('''
   ____ ___ ____  ____  ____            _          
  / ___|_ _|  _ \|  _ \|  _ \ _ __ ___ | |__   ___ 
 | |    | || | | | |_) | |_) | '__/ _ \| '_ \ / _ \
 | |___ | || |_| |  _ <|  __/| | | (_) | |_) |  __/
  \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|
                                                   
-For any assistance, please feel free to contact me via Telegram:
-Telegram: https://t.me/brian_72
+https://t.me/brian_72
+contcat me via telegram for any issues 
 ''')
     main()
