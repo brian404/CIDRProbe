94cc26b01275826aa744df048a48d3f5889db1ef
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 14:29:47 +0300

    /

--- cidr.py
+++ cidr.py
@@ -1,77 +1,33 @@
-import sys
+import ipaddress
 import requests
 from termcolor import colored
-from tabulate import tabulate
 
-BANNER = '''
-  ____ ___ ____  ____  ____            _    
- / ___|_ _|  _ \|  _ \|  _ \ _ __ ___ | |__ 
-| |    | || | | | |_) | |_) | '__/ _ \| '_ \\
-| |___ | || |_| |  _ <|  __/| | | (_) | |_) |
- \____|___|____/|_| \_\_|   |_|  \___/|_.__/
-
-CIDR Probe - Scan CIDR Range
-
-'''
-
-CDN_PROVIDERS = {
-    "cloudfront.net": "CloudFront",
-    "cloudflare": "Cloudflare",
-    # Add more CDN providers here
-}
-
-def detect_cdn(url):
-    for domain, provider in CDN_PROVIDERS.items():
-        if domain in url:
-            return provider
-    return ""
+def scan_cidr(cidr, port=80):
+    ip_network = ipaddress.ip_network(cidr)
+    for ip in ip_network.hosts():
+        url = f"http://{str(ip)}"
+        try:
+            response = requests.get(url, timeout=1)
+            status = response.status_code
+            if status == 200:
+                status_text = colored("200 OK", "green")
+            else:
+                status_text = colored(f"{status}", "purple")
+        except requests.exceptions.RequestException:
+            status_text = colored("Failed to connect", "red")
+        print(f"{ip:<15}{port:<7}{status_text}")
 
 def main():
-    print(BANNER)
-    
-    if len(sys.argv) > 1 and sys.argv[1] == "-p":
-        print("Usage: python cidr.py [<CIDR>]")
-        print("Example: python cidr.py 10.0.0.0/24")
-        return
-    
+    print("CIDR Probe - Scan CIDR Range\n")
     cidr = input("Enter the CIDR range: ")
     port = input("Enter the port to scan (default is 80): ")
-    port = int(port) if port else 80
-    
+    if not port:
+        port = 80
     print(f"\nScanning CIDR range: {cidr}")
     print(f"Port: {port}\n")
-    
-    headers = ["IP", "Port", "Status", "CDN"]
-    rows = []
-    
-    try:
-        ips = cidr.split('/')[0]
-        prefix = int(cidr.split('/')[1])
-        start_ip = sum([int(x) << y for x, y in zip(ips.split('.'), [24, 16, 8, 0])])
-        end_ip = start_ip + 2 ** (32 - prefix)
-        
-        for ip in range(start_ip, end_ip):
-            ip_address = '.'.join([str(ip >> (y << 3) & 0xFF) for y in [3, 2, 1, 0]])
-            url = f"http://{ip_address}:{port}"
-            
-            try:
-                response = requests.get(url, timeout=5)
-                
-                if response.status_code == 200:
-                    status = colored("Open", "green")
-                else:
-                    status = colored(response.status_code, "purple")
-                
-                cdn = detect_cdn(url)
-                rows.append([ip_address, port, status, cdn])
-            except requests.exceptions.RequestException:
-                rows.append([ip_address, port, colored("Failed to connect", "red"), ""])
-    
-    except KeyboardInterrupt:
-        print("\n\nScan interrupted by user.")
-    
-    print(tabulate(rows, headers=headers))
-
+    print("IP             Port   Status      CDN")
+    print("--------------------------------------")
+    scan_cidr(cidr, port)
 
 if __name__ == "__main__":
     main()
