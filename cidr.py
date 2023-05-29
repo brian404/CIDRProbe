c48492cd08c213514e765bb5920710112fe64480
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 15:37:08 +0300

    /

--- cidr.py
+++ cidr.py
@@ -1,45 +1,42 @@
 import ipaddress
-import nmap
-import requests
-from termcolor import colored
+import socket
 
 def scan_cidr(cidr, port):
-    nm = nmap.PortScanner()
     ip_network = ipaddress.ip_network(cidr)
-    
-    print(f"\nScanning CIDR range: {cidr}")
+    print(f"Scanning CIDR range: {cidr}")
     print(f"Port: {port}\n")
-    print("IP\t\tPort\tStatus\t\tHTTP Response")
+    print("IP\t\tPort\tStatus")
     print("--------------------------------------")
 
     for ip in ip_network.hosts():
+        ip_str = str(ip)
         try:
-            target = str(ip)
-            nm.scan(target, arguments=f"-p {port} --open")
-            if nm[target].has_tcp(int(port)) and nm[target].tcp(int(port))['state'] == 'open':
-                status_text = colored("Open", "green")
-                response = requests.get(f"http://{target}:{port}")
-                http_status = response.status_code
-            else:
-                status_text = colored("Closed", "red")
-                http_status = ""
-            print(f"{ip}\t{port}\t{status_text}\t\t{http_status}")
-        except KeyboardInterrupt:
-            print("\nScan interrupted by user.")
-            break
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.settimeout(2)  # Set a timeout for the connection attempt
+                result = sock.connect_ex((ip_str, port))
+                if result == 0:
+                    print(f"{ip_str}\t{port}\tOpen")
+                else:
+                    print(f"{ip_str}\t{port}\tClosed")
+        except socket.error:
+            print(f"{ip_str}\t{port}\tUnable to scan")
 
 def main():
-    print("  ____ ___ ____  ____  ____            _          ")
-    print(" / ___|_ _|  _ \|  _ \|  _ \ _ __ ___ | |__   ___ ")
-    print("| |    | || | | | |_) | |_) | '__/ _ \| '_ \ / _ \\")
-    print("| |___ | || |_| |  _ <|  __/| | | (_) | |_) |  __/")
-    print(" \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|")
-    print("\nhttps://t.me/brian_72\n")
-    print("CIDR Probe - Scan CIDR Range")
-    print("\n")
     cidr = input("Enter the CIDR range: ")
-    port = input("Enter the port to scan (default is 80): ") or "80"
+    port = input("Enter the port to scan (default is 80): ") or 80
+    port = int(port)
+
     scan_cidr(cidr, port)
 
 if __name__ == "__main__":
+    print(r'''
+  ____ ___ ____  ____  ____            _          
+ / ___|_ _|  _ \|  _ \|  _ \ _ __ ___ | |__   ___ 
+| |    | || | | | |_) | |_) | '__/ _ \| '_ \ / _ \
+| |___ | || |_| |  _ <|  __/| | | (_) | |_) |  __/
+ \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|
+                                                  
+For any assistance, please feel free to contact me via Telegram:
+Telegram: https://t.me/brian_72
+''')
     main()
