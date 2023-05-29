73e6f1d08def6cfbfe5f9f02656bc70cd958fb23
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 13:35:24 +0300

    /

--- cidr.py
+++ cidr.py
@@ -1,81 +1,58 @@
+import argparse
 import ipaddress
 import requests
-import argparse
-import socket
+from requests.exceptions import RequestException
 from termcolor import colored
-import time
 
-BANNER = r"""
-  ____ ___ ____  ____  ____            _          
- / ___|_ _|  _ \|  _ \|  _ \ _ __ ___ | |__   ___ 
-| |    | || | | | |_) | |_) | '__/ _ \| '_ \ / _ \
-| |___ | || |_| |  _ <|  __/| | | (_) | |_) |  __/
- \____|___|____/|_| \_\_|   |_|  \___/|_.__/ \___|
-"""
-
-def is_port_open(ip, port):
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.settimeout(1)
-            result = sock.connect_ex((ip, port))
-            if result == 0:
-                return True
-            else:
-                return False
-    except socket.error:
-        return False
-
-def scan_cidr(cidr, port, timeout):
+def scan_cidr(cidr, port):
     try:
         network = ipaddress.ip_network(cidr)
     except ValueError:
-        print("Invalid CIDR range entered. Please try again.")
+        print("Invalid CIDR range. Please provide a valid CIDR range.")
         return
 
-    alive_ips = []
-
-    print(BANNER)
-    print(f"\nScanning CIDR range: {cidr}\n")
-
+    print(f"Scanning CIDR range: {cidr}")
     for ip in network.hosts():
-        ip_str = str(ip)
-
         try:
-            response = requests.get(f"http://{ip_str}", timeout=timeout)
-            if response.status_code == requests.codes.ok:
-                if port and is_port_open(ip_str, port):
-                    alive_ips.append(ip_str)
-                    print(colored(f"Alive IP: {ip_str}, Response: {response.status_code}", "green"))
-                else:
-                    print(colored(f"Non-alive IP: {ip_str}, Response: {response.status_code}", "red"))
+            url = f"http://{ip}:{port}"
+            response = requests.get(url, timeout=1)
+            if response.status_code == 200:
+                print(f"{ip} - {colored('[+]', 'green')} Status: {response.status_code}")
             else:
-                if port and is_port_open(ip_str, port):
-                    print(colored(f"Alive IP (HTTP error): {ip_str}, Response: {response.status_code}", "green"))
-                else:
-                    print(colored(f"Non-alive IP (HTTP error): {ip_str}, Response: {response.status_code}", "red"))
-        except requests.exceptions.RequestException:
-            if port and is_port_open(ip_str, port):
-                print(colored(f"Alive IP (Connection error): {ip_str}", "green"))
-            else:
-                print(colored(f"Non-alive IP (Connection error): {ip_str}", "red"))
+                print(f"{ip} - {colored('[-]', 'red')} Status: {response.status_code}")
+        except RequestException:
+            print(f"{ip} - {colored('[-]', 'red')} Failed to connect")
 
-        time.sleep(0.1)  # Delay for better visualization
+    print("Scan completed.")
 
-    if alive_ips:
-        with open("alive.txt", "w") as file:
-            file.write("\n".join(alive_ips))
-        print("\nAlive IP addresses saved to alive.txt")
-    else:
-        print("\nNo alive IP addresses found within the specified CIDR range.")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="CIDRProbe - Advanced CIDR Scanner")
+    parser.add_argument("cidr", type=str, help="CIDR range to scan")
+    parser.add_argument("-p", "--port", nargs="+", type=int, default=[80],
+                        help="Port(s) to scan (default: 80)")
+    return parser.parse_args()
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Advanced CIDR Scanner")
-    parser.add_argument("cidr", type=str, help="CIDR range to scan")
-    parser.add_argument("-p", "--port", type=str, help="Port(s) to check")
-    parser.add_argument("-t", "--timeout", type=int, default=1, help="Timeout for HTTP requests (default: 1 second)")
-    args = parser.parse_args()
+    banner = r"""
+  ____ ___ ____  ____  ____
+ / ___|_ _|  _ \|  _ \|  _ \ _ __ ___
+| |    | || | | | |_) | |_) | '__/ _ \
+| |___ | || |_| |  _ <|  __/| | | (_) |
+ \____|___|____/|_| \_\_|   |_|  \___/
 
-    scan_cidr(args.cidr, args.port, args.timeout)
+CIDRProbe - Advanced CIDR Scanner
+    """
+    print(banner)
+
+    args = parse_arguments()
+    cidr = args.cidr
+    ports = args.port
+
+    for port in ports:
+        scan_cidr(cidr, port)
+
 
 if __name__ == "__main__":
     main()
