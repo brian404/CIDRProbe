d7315c921d8a8336c028e00506b27b8837ee4932
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 13:53:12 +0300

    /

--- cidr.py
+++ cidr.py
@@ -1,58 +1,58 @@
-import argparse
 import ipaddress
 import requests
-from requests.exceptions import RequestException
-from termcolor import colored
+import signal
+import sys
+from colorama import Fore, Style
+
+# Banner
+banner = '''
+  ____ ___ ____  ____  ____            _    
+ / ___|_ _|  _ \|  _ \|  _ \ _ __ ___ | |__ 
+| |    | || | | | |_) | |_) | '__/ _ \| '_ \\
+| |___ | || |_| |  _ <|  __/| | | (_) | |_) |
+ \____|___|____/|_| \_\_|   |_|  \___/|_.__/ 
+'''
+
+# Your GitHub link
+github_link = 'https://github.com/brian404/'
 
 def scan_cidr(cidr, port):
     try:
         network = ipaddress.ip_network(cidr)
-    except ValueError:
-        print("Invalid CIDR range. Please provide a valid CIDR range.")
-        return
+        print(f"\nScanning CIDR range: {cidr} on port {port}\n")
 
-    print(f"Scanning CIDR range: {cidr}")
-    for ip in network.hosts():
-        try:
+        # Handle Ctrl+C to cancel the operation
+        def signal_handler(signal, frame):
+            print("\nOperation cancelled by user.")
+            sys.exit(0)
+
+        signal.signal(signal.SIGINT, signal_handler)
+
+        for ip in network.hosts():
             url = f"http://{ip}:{port}"
-            response = requests.get(url, timeout=1)
-            if response.status_code == 200:
-                print(f"{ip} - {colored('[+]', 'green')} Status: {response.status_code}")
-            else:
-                print(f"{ip} - {colored('[-]', 'red')} Status: {response.status_code}")
-        except RequestException:
-            print(f"{ip} - {colored('[-]', 'red')} Failed to connect")
+            try:
+                response = requests.get(url, timeout=1)
+                if response.status_code == 200:
+                    print(f"[+] {ip} - Port {port}: {Fore.GREEN}Open{Style.RESET_ALL}")
+                else:
+                    print(f"[-] {ip} - Port {port}: {Fore.MAGENTA}Closed{Style.RESET_ALL}")
+            except requests.exceptions.RequestException as e:
+                print(f"[-] {ip} - Failed to connect: {e}")
 
-    print("Scan completed.")
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="CIDRProbe - Advanced CIDR Scanner")
-    parser.add_argument("cidr", type=str, help="CIDR range to scan")
-    parser.add_argument("-p", "--port", nargs="+", type=int, default=[80],
-                        help="Port(s) to scan (default: 80)")
-    return parser.parse_args()
-
+    except ValueError as e:
+        print("Invalid CIDR range. Please provide a valid range.")
 
 def main():
-    banner = r"""
-  ____ ___ ____  ____  ____
- / ___|_ _|  _ \|  _ \|  _ \ _ __ ___
-| |    | || | | | |_) | |_) | '__/ _ \
-| |___ | || |_| |  _ <|  __/| | | (_) |
- \____|___|____/|_| \_\_|   |_|  \___/
-
-CIDRProbe - Advanced CIDR Scanner
-    """
     print(banner)
+    print(f"GitHub: {github_link}\n")
 
-    args = parse_arguments()
-    cidr = args.cidr
-    ports = args.port
+    cidr = input("Enter the CIDR range to scan (e.g., 192.168.0.0/24): ")
+    port = input("Enter the port number to scan (default is 80): ")
 
-    for port in ports:
-        scan_cidr(cidr, port)
+    if not port:
+        port = 80
 
+    scan_cidr(cidr, port)
 
 if __name__ == "__main__":
     main()
