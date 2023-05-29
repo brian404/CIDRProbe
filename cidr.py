c1a496276df1dd7f0c6381fdac498d3477744543
bmunene404@gmail.com <bmunene404@gmail.com>
2023-05-29 14:55:27 +0300

    /

--- cidr.py
+++ cidr.py
@@ -2,44 +2,31 @@ import ipaddress
 import requests
 from termcolor import colored
 
-def scan_cidr(cidr, port=80):
+def scan_cidr(cidr, port):
     ip_network = ipaddress.ip_network(cidr)
-    for ip in ip_network.hosts():
-        url = f"http://{str(ip)}"
-        try:
-            response = requests.get(url, timeout=1)
-            status = response.status_code
-            if status == 200:
-                status_text = colored("200 OK", "green")
-                port_text = colored(port, "green")
-            else:
-                status_text = colored(f"{status}", "magenta")  # Use magenta as a fallback color
-                port_text = colored(port, "red")
-        except requests.exceptions.RequestException:
-            status_text = colored("Failed to connect", "red")
-            port_text = colored(port, "red")
-        print(f"{str(ip):<15} {port_text:<7} {status_text}")
-
-def main():
-    print("CIDR Probe - Scan CIDR Range\n")
-    while True:
-        cidr = input("Enter the CIDR range (e.g., 192.168.0.0/24): ")
-        try:
-            ip_network = ipaddress.ip_network(cidr)
-            break
-        except ValueError:
-            print("Invalid CIDR range. Please try again.\n")
-    port = input("Enter the port to scan (default is 80): ")
-    if not port:
-        port = 80
+    
     print(f"\nScanning CIDR range: {cidr}")
     print(f"Port: {port}\n")
-    print("IP             Port   Status      CDN")
+    print("IP\t\tPort\tStatus")
     print("--------------------------------------")
-    try:
-        scan_cidr(cidr, port)
-    except KeyboardInterrupt:
-        print("\nScan interrupted by user.")
+
+    for ip in ip_network.hosts():
+        try:
+            url = f"http://{ip}:{port}"
+            response = requests.get(url, timeout=1)
+            status = "Open" if response.status_code == 200 else "Closed"
+            status_text = colored(status, "green" if status == "Open" else "red")
+            print(f"{ip}\t{port}\t{status_text}")
+        except requests.exceptions.RequestException:
+            print(f"{ip}\t{port}\tFailed to connect")
+        except KeyboardInterrupt:
+            print("\nScan interrupted by user.")
+            break
+
+def main():
+    cidr = input("Enter the CIDR range: ")
+    port = input("Enter the port to scan (default is 80): ") or "80"
+    scan_cidr(cidr, port)
 
 if __name__ == "__main__":
     main()
