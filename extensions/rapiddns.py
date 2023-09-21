a36a3fdd4f2f336733604867a4a93c9bd3fe265c
bmunene404@gmail.com <bmunene404@gmail.com>
2023-09-21 21:37:25 +0300

    update rapiddns

--- extensions/rapiddns.py
+++ extensions/rapiddns.py
@@ -1,29 +1,40 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def reverse_ip_lookup(ip_address):
+def reverse_ip_lookup_selenium(ip_address):
     url = f"https://rapiddns.io/sameip/{ip_address}#result"
 
     try:
-        response = requests.get(url)
+        # Set up the WebDriver (download the appropriate WebDriver for your browser)
+        driver = webdriver.Chrome()  # Example for Chrome; adjust as needed
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            domain_elements = soup.find_all('td', class_='ellipsis')
-            domains = [element.get_text() for element in domain_elements]
-            return domains
+        # Open the URL
+        driver.get(url)
 
-        else:
-            return None
+        # Wait for the data to load (adjust the timeout as needed)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'ellipsis')))
+
+        # Extract the domain names
+        domain_elements = driver.find_elements(By.CLASS_NAME, 'ellipsis')
+        domains = [element.text for element in domain_elements]
+
+        return domains
 
     except Exception as e:
         print(f"Error: {str(e)}")
         return None
 
+    finally:
+        # Close the WebDriver when done
+        driver.quit()
+
 if __name__ == "__main__":
     ip_address = input("Enter the IP address for reverse lookup: ")
 
-    result = reverse_ip_lookup(ip_address)
+    result = reverse_ip_lookup_selenium(ip_address)
     if result:
         for domain in result:
             print(domain)
