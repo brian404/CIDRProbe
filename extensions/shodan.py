19a1b4dbea37fd9f1a04dab145f88d5e757f27f0
bmunene404@gmail.com <bmunene404@gmail.com>
2023-09-21 22:02:34 +0300

    shodan

---
+++ extensions/shodan.py
@@ -0,0 +1,23 @@
+import requests
+
+SERVER_ENDPOINT = "http://endpoint.brian72.eu.org:5000/get_api_key"
+
+def get_shodan_api_key():
+    try:
+        response = requests.get(f"{SERVER_ENDPOINT}?api_name=shodan")
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("api_key")
+        else:
+            print("Failed to fetch Shodan API key.")
+            return None
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+
+shodan_api_key = get_shodan_api_key()
+if shodan_api_key:
+    print(f"Using Shodan API key: {shodan_api_key}")
+    # Perform your Shodan queries here
+else:
+    print("Shodan API key not available.")
